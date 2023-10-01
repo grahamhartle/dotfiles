7c51ed8481c10a85dc2ba0e1aaff49d541bf35e4
Graham Hartle <ghartle@me.com>
2023-10-01 11:21:07 +1100

    testing alias

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,5 @@ local config = {
 	font = wezterm.font("Hack Nerd Font", { weight = "Regular", italic = false }),
 	line_height = 1,
 	window_background_opacity = 0.80,
-	initial_rows = 30,
-	initial_cols = 90,
 }
 return config
