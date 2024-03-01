0f6bc208983d81d94c2a030cb1b29cccbd52b916
Graham Hartle <ghartle@me.com>
2024-03-02 00:14:55 +1100

    general update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local config = {
-	color_scheme = "MaterialDesignColors",
+	color_scheme = "Catppuccin Frappe",
 	font_size = 13,
 	font = wezterm.font("Hack Nerd Font", { weight = "Regular", italic = false }),
 	line_height = 1,
