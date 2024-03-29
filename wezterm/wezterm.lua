8f3694eeb03a8966b2fe0089444522085031dcf3
Graham Hartle <ghartle@me.com>
2024-03-29 11:28:17 +1100

    update files

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = {
 	initial_cols = 220,
 	initial_rows = 60,
 	window_decorations = "NONE",
-	color_scheme = "Catppuccin Frappe",
+	color_scheme = "Catppuccin Mocha",
 	font_size = 13,
 	font = wezterm.font("Hack Nerd Font", { weight = "Regular", italic = false }),
 	line_height = 1,
