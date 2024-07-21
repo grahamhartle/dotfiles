ae51f0cd9e66509148f303315b6f331abd886318
grahamhartle <ghartle@me.com>
2024-07-21 15:12:47 +1000

    file updates

--- zsh/.config/zsh/scripts/git-workflow.sh
+++ zsh/.config/zsh/scripts/git-workflow.sh
@@ -5,12 +5,12 @@
 commit_message="$1"
 
 # Check if the current directory is a git repo
-if [ $(git rev-parse --is-inside-work-tree) = false ]; then
-    echo "This is not a Git Repo!"
-else
+if [ $(git rev-parse --is-inside-work-tree) = true ]; then
     git add .
     git commit -m "$commit_message"
     git push
     echo "Changes commited and pushed with message: $commit_message"
+else
+    echo "This is not a Git Repo!"
 fi
 
