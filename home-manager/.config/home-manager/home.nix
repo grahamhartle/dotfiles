aabc0671123b01c3e18f653cac0d41eed452fade
grahamhartle <ghartle@me.com>
2024-08-29 18:44:57 +1000

    wezterm

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -35,6 +35,7 @@
     tmux
     nano
     kitty
+    wezterm
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
