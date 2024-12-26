1e37c0a339b01ead5e702266e8eac51882944058
Graham Hartle <graham@Grahams-Mini.modem>
2024-12-26 16:42:20 +1100

    updated nushell config

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -4,3 +4,9 @@ hs.hotkey.bind( super, 'h', function()
   hs.notify.new({title = "Hammerspoon", informativeText = "Hello, world!"}):send()
 end)
 
+hs.hotkey.bind( super, 'r', function ()
+  hs.reload()
+end)
+
+hs.alert.show("Config reloaded")
+
