6ecde28b4b32d6dce0cf0a21f6b9f872efba3197
Graham Hartle <ghartle@icloud.com>
2025-02-02 12:01:34 +1100

    updated files

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -4,6 +4,9 @@ hs.hotkey.bind( super, 'h', function()
   hs.notify.new({title = "Hammerspoon", informativeText = "Hello, world!"}):send()
 end)
 
+hs.loadSpoon("WindowScreenLeftAndRight")
+spoon.WindowScreenLeftAndRight:bindHotkeys(spoon.WindowScreenLeftAndRight.defaultHotkeys)
+
 hs.hotkey.bind( super, 'r', function ()
   hs.reload()
 end)
--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -1,2 +1,5 @@
 source ~/.config/zsh/zshrc
 
+
+# Created by `pipx` on 2024-12-31 23:51:29
+export PATH="$PATH:/Users/graham/.local/bin"
